789c1bd800b5ca56102632c0b0aa5113c3fd72ef
eduairet <hola@eduairet.com>
2022-06-06 19:57:30 -0500

    Homework 1

---
+++ eduairet/homework001/script.js
@@ -0,0 +1,11 @@
+// Link this file to the HTML file
+let js = 'Devs2Rios';
+let emptyVar;
+if (js=== 'Devs2Rios') {alert('JavaScript is 🔥')};
+console.log(5+10*30);
+console.log(js);
+console.log(js, js);
+console.log(true); // Boolean
+console.log(typeof 30);
+console.log(typeof emptyVar);
+console.log(typeof null); // Object for legacy
